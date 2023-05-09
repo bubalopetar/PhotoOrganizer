c32356426dfc8464cff870e05727c071f308ccee
bubalopetar <bubalopetar95@gmail.com>
2023-05-09 16:23:50 +0200

    rename Organizer class

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-import 'organizer.dart';
+import 'photo_organizer.dart';
 
 void main() {
   String photos = """DSC012333.jpg, Madrid, 2016-10-01 13:02:34
@@ -8,6 +8,6 @@ DSC044322.jpeg, Milan, 2015-03-04 14:55:01
 DSC130033.jpg, Rio, 2018-06-02 17:05:10
 DSC012335.jpg, Milan, 2015-03-05 10:11:24""";
 
-  Organizer organizer = Organizer(photos);
+  PhotoOrganizer organizer = PhotoOrganizer(photos);
   print(organizer.getOrganizedPhotos());
 }
